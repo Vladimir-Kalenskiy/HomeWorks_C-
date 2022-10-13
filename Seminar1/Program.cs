6f855db9e68a387d96bda0fbe03d8fe3d9f70c69
Владимир Каленский <kalenskiy.man@icloud.com>
2022-10-13 21:35:36 +0300

    Решали 3 задачи

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿// int - множество целых чисел. double - уточнение на данные после запятой. string - строка, текст. 
+// char - единичные символы. bool - логическая величина (true/false).
+
+/* Задача 1. Текст задачи.
+Console.Write("Input first a number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second a number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = n2 * n2;
+
+if(quad2 == n1)
+{
+    Console.WriteLine($"Number {n1} is square of {n2}");
+}
+else
+{
+    Console.WriteLine($"Number {n1} is not square of {n2}");
+}
+*/
+
+// Задача 2.
+
+/*Console.Write("Input a pasitive integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = - number;
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+//Задача 3. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+/*
+Console.Write("Input three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num % 10;
+Console.WriteLine($"Last digit of {num} is {result}");
+*/
